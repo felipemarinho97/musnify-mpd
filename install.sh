5fb3950f695a8f3172478cea500d513ae64e659e
Felipe Marinho <felipevm97@gmail.com>
2017-04-04 23:29:54 -0300

    fix

--- install.sh
+++ install.sh
@@ -8,8 +8,10 @@ chmod 755 /usr/bin/musnify-mpd
 echo 'copying config files'
 cp musnify-mpd.config /etc/musnify-mpd.config
 echo 'config file stored in /etc/musnify-mpd.config'
+
 mkdir /usr/share/doc/musnify-mpd
 cp doc/musnify-mpdconfig.example /usr/share/doc/musnify-mpd/musnify-mpdconfig.example
+chmod 555 /usr/share/doc/musnify-mpd/musnify-mpdconfig.example
 echo 'copy the musnify-mpdconfig.example to ~/.config/musnify-mpd/musnify-mpd.config to set up your custom config'
 
 cp README.md /usr/share/doc/musnify-mpd/README.md
