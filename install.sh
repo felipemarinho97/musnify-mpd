8a9264dc240400bb271b358bbc7b87500686d1f6
Felipe Marinho <felipevm97@gmail.com>
2017-04-04 22:27:39 -0300

    creating doc/ directory

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo 'copying config files'
 cp musnify-mpd.config /etc/musnify-mpd.config
 echo 'config file stored in /etc/musnify-mpd.config'
 mkdir /etc/usr/share/doc/musnify-mpd
-cp musnify-mpdconfig.example /etc/usr/share/doc/musnify-mpd/musnify-mpdconfig.example
+cp doc/musnify-mpdconfig.example /etc/usr/share/doc/musnify-mpd/musnify-mpdconfig.example
 echo 'copy the musnify-mpdconfig.example to ~/.config/musnify-mpd/musnify-mpd.config to set up your custom config'
 
 cp README.md /etc/usr/share/doc/musnify-mpd/README.md
